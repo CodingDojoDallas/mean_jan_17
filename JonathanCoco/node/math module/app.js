76ca4e1f0ffef5eea37d12c7ed9a0d6f6d8b55d9
Jonathan Coco <cocojonathan@yahoo.com>
2017-01-11 08:17:53 -0600

    node

---
+++ JonathanCoco/node/math module/app.js
@@ -0,0 +1,6 @@
+var my_module = require('./mathlib')();     //notice the extra invocation parentheses!
+console.log(my_module);
+my_module.add(5,6);
+my_module.multiply(7,5);
+my_module.square(5);
+my_module.random(5,100);
